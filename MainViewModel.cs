47f0bb4c946e5e0dddf814d674a1e06423accf0a
trofimchik <trofkon93@gmail.com>
2020-10-06 12:05:15 +0300

    Add project files.

---
+++ MainViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Data;
+
+namespace DeleteThisExample
+{
+    class MainViewModel
+    {
+        public ICollectionView CollectionViewZero { get; private set; }
+        public ICollectionView CollectionViewOne { get; private set; }
+        public ObservableCollection<Model> Nums { get; private set; } = new ObservableCollection<Model>();
+        public string RandomProp { get; set; }
+        public int AnotherRandomProp { get; set; }
+
+        public MainViewModel()
+        {
+            Nums.Add(new Model(1));
+            Nums.Add(new Model(0));
+            Nums.Add(new Model(1));
+            Nums.Add(new Model(1));
+            Nums.Add(new Model(0));
+
+            CollectionViewZero = new CollectionViewSource { Source = Nums }.View;
+            CollectionViewZero.Filter = ReturnZeroFilter;
+            CollectionViewOne = new CollectionViewSource { Source = Nums }.View;
+            CollectionViewOne.Filter = ReturnOneFilter;
+        }
+
+        private bool ReturnOneFilter(object item)
+        {
+            Model temp = item as Model;
+            if (temp.Num == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+        private bool ReturnZeroFilter(object item)
+        {
+            Model temp = item as Model;
+            if (temp.Num == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Model.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeleteThisExample
+{
+    class Model
+    {
+        public int Num { get; set; }
+        public Model(int num)
+        {
+            Num = num;
+        }
+    }
+}
